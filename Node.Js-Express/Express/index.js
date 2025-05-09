fca8a6a909334683cfe3336f56def34f9c224fbb
Saif <saifullahlatki338@gmail.com>
2025-05-10 01:52:34 +0500

    start express

---
+++ Node.Js-Express/Express/index.js
@@ -0,0 +1,42 @@
+const express= require('express');
+const app= express();
+
+const courses=[
+    {id:1,name:'Database'},
+    {id:2,name:'computer networking'},
+    {id:3,name:'Web Development'}
+];
+app.get('/',(req,res)=>{
+    res.send('Hello World!')
+});
+app.get('/api/users',(req,res)=>{
+    res.send([
+        {id:1,name:'John'},
+        {id:2,name:'Jane'},
+        {id:3,name:'Doe'}
+    ]);
+});
+app.get('/api/courses',(req,res)=>{
+    res.send(courses);
+});
+app.get('/api/courses/:id',(req,res)=>{
+    const course= courses.find(c=>c.id===parseInt(req.params.id));
+    if(!course) return res.status(404).send('The course with the given ID was not found.');
+    res.send(course);
+});
+//route parameters
+// app.get('/api/users/:id',(req,res)=>{
+//     res.send(req.params.id);
+// });
+// app.get('/api/posts/:year/:month',(req,res)=>{
+//     res.send(req.params);
+// });
+//query parameters
+app.get('/api/posts/:year/:month',(req,res)=>{
+    res.send(req.query);
+});
+//PORT
+const port= process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
